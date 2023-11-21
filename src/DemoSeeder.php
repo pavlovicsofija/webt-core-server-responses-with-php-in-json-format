17bfc1133ccecb020c619c60d57adf2423e0f991
oroz_lorena <0053@htl.rennweg.at>
2023-11-21 23:59:41 +0100

    OST uund Song klassen, Seed funktion erstellt aber nicht ausgeführt

--- src/DemoSeeder.php
+++ src/DemoSeeder.php
@@ -1,1 +1,16 @@
 <?php
+class DemoSeeder{
+    public static function seed(){
+        $demos = array();
+
+        for ($i = 0; $i < 3; $i++) {
+            for ($j = 0; $j < 4; $j++) {
+                $tracks[] = new Song($j, "Song $j", "Artist $j", $j, rand(120, 240));
+           }
+            $demos[$i] = new OST($i, "ost $i", "vgname0 $i", 2000 + $i, $tracks);
+        }
+        return $demos;
+    }
+}
+
+
--- src/OST.php
+++ src/OST.php
@@ -1,1 +1,208 @@
 <?php
+class OST{
+    private $id;
+    private $name;
+    private $vgname;
+    private $year;
+    private $tracklist;
+
+    /**
+     * @param $id
+     * @param $name
+     * @param $vgname
+     * @param $year
+     * @param $tracklist
+     */
+    public function __construct($id, $name, $vgname, $year, $tracklist)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->vgname = $vgname;
+        $this->year = $year;
+        $this->tracklist = $tracklist;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVgname()
+    {
+        return $this->vgname;
+    }
+
+    /**
+     * @param mixed $vgname
+     */
+    public function setVgname($vgname)
+    {
+        $this->vgname = $vgname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param mixed $year
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTracklist()
+    {
+        return $this->tracklist;
+    }
+
+    /**
+     * @param mixed $tracklist
+     */
+    public function setTracklist($tracklist)
+    {
+        $this->tracklist = $tracklist;
+    }
+}
+
+class Song{
+    private $uid;
+    private $sname;
+    private $artist;
+    private $tracknum;
+    private $duration;
+
+    /**
+     * @param $uid
+     * @param $sname
+     * @param $artist
+     * @param $tracknum
+     * @param $duration
+     */
+    public function __construct($uid, $sname, $artist, $tracknum, $duration)
+    {
+        $this->uid = $uid;
+        $this->sname = $sname;
+        $this->artist = $artist;
+        $this->tracknum = $tracknum;
+        $this->duration = $duration;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+    /**
+     * @param mixed $uid
+     */
+    public function setUid($uid)
+    {
+        $this->uid = $uid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSname()
+    {
+        return $this->sname;
+    }
+
+    /**
+     * @param mixed $sname
+     */
+    public function setSname($sname)
+    {
+        $this->sname = $sname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * @param mixed $artist
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTracknum()
+    {
+        return $this->tracknum;
+    }
+
+    /**
+     * @param mixed $tracknum
+     */
+    public function setTracknum($tracknum)
+    {
+        $this->tracknum = $tracknum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param mixed $duration
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+    }
+}
