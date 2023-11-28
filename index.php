2f999a9990c6f8c00a4a1672338bdd17805d7787
oroz_lorena <0053@htl.rennweg.at>
2023-11-28 22:49:04 +0100

    glaube fertig

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once ('src/DemoSeeder.php');
+require_once ('src/Song.php');
 require_once ('src/OST.php');
 
 header("Content-Type: application/json");
@@ -7,20 +8,26 @@ header("Content-Type: application/json");
 $demos = DemoSeeder::seed();
 
 //var_dump($demos);
-
 //echo $demos[0]->getId();
-
-
 //echo htmlspecialchars($_GET['ost_id']);
-$gesuchteOst = ($_GET['ost_id']);
+
+
+
+//intval, damit sichergestellt wird, dass es eine Ganzzahl ist, so würde es auch gehen -> $gesuchteOst = ($_GET['ost_id']);
+$gesuchteOst = intval($_GET['ost_id']);
+
 //echo $gesuchteOst;
 
 
-
-for ($i = 0; $i < count($demos); $i++) {
+//schleife die jede OST ID durchgeht und mit $gesuchteOst vergleicht, wenn es matched wird es ausgegeben
+for ($i = 1; $i < count($demos); $i++) {
     if ($gesuchteOst == $demos[$i]-> getId()){
-        echo json_encode($demos[$i]);
+        echo json_encode($demos[$i], JSON_PRETTY_PRINT);
          //var_dump($demos[$i]);
+}
     }
+
+for ($i = 1; $i < count($demos); $i++) {
+    echo json_encode($demos[$i], JSON_PRETTY_PRINT);
 }
 
--- src/DemoSeeder.php
+++ src/DemoSeeder.php
@@ -3,16 +3,14 @@ class DemoSeeder{
     public static function seed(){
         $demos = array();
 
-        for ($i = 0; $i < 3; $i++) {
-            for ($j = 0; $j < 4; $j++) {
-                $tracks[] = new Song($j, "Song $j", "Artist $j", $j, rand(120, 240));
-           }
+        for ($i = 1; $i < 4; $i++) {
+            $tracks = array();
+            for ($j = 1; $j < 5; $j++) {
+                $song = new Song($j, "Song $j", "Artist $j", $j, rand(120, 240));
+                    array_push($tracks, $song);
+                }
             $demos[$i] = new OST($i, "ost $i", "vgname $i", 2000 + $i, $tracks);
         }
         return $demos;
     }
 }
-
-
-
-
